480eeb24c306ff55ea1f77d8ddc0a76abb662494
Eicoma <328084834@qq.com>
2021-04-27 21:43:21 +0800

    Demo1 JDBCUtils 完成

---
+++ jdbc-basis/src/eicoma/com/github/jdbc/Demo1/utils/JDBCUtils.java
@@ -0,0 +1,98 @@
+package eicoma.com.github.jdbc.Demo1.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+public class JDBCUtils {
+    //私有空参构造方法
+    private JDBCUtils(){}
+
+    //声明配置信息变量
+    private static String driverClass;
+    private static String url;
+    private static String username;
+    private static String password;
+    private static Connection con;
+
+    static{
+        try {
+            InputStream is = JDBCUtils.class.getClassLoader().getResourceAsStream("config.properties");
+            //创建Properties集合
+            //Properties集合是具有键值对的双列集合
+            Properties prop = new Properties();
+            // 通过Properties集合来加载流对象
+            //将配置文件中的内容读取到内存中
+            prop.load(is);
+
+            //通过Properties集合的getProperty()方法来获取键对应的值
+            driverClass = prop.getProperty("driverClass");
+            url = prop.getProperty("url");
+            username = prop.getProperty("username");
+            password = prop.getProperty("password");
+
+            //通过变量driverClass注册驱动
+            Class.forName(driverClass);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection(){
+        try {
+            con = DriverManager.getConnection(url,username,password);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return con;
+    }
+
+    public static void close(Connection con, Statement stat, ResultSet rs){
+        if (con!=null){
+            try {
+                con.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        if (stat!=null){
+            try {
+                stat.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        if (rs !=null){
+            try {
+                rs.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+    public static void close(Connection con, Statement stat){
+        if (con!=null){
+            try {
+                con.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        if (stat!=null){
+            try {
+                stat.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+
+}
