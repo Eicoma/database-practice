f615004ce8b34c9a5a4144695be947eca3e50498
Eicoma <328084834@qq.com>
2021-05-16 13:54:08 +0800

    ResponseDemo2

--- response-demo/src/eicoma/com/github/responsedemo/ResponseDemo2.java
+++ response-demo/src/eicoma/com/github/responsedemo/ResponseDemo2.java
@@ -1,6 +1,5 @@
 package eicoma.com.github.responsedemo;
 
-import javax.jws.WebResult;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
@@ -8,24 +7,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
- * 字节流响应消息
+ * 字符流响应消息
  */
 
-@WebServlet("/responseDemo1")
-public class ResponseDemo1 extends HttpServlet {
+@WebServlet("/responseDemo2")
+public class ResponseDemo2 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //设置客户端浏览器的字符集
+        String str = "字符流响应消息";
+        //设置 浏览器解码字符集，解决中文乱码
+        //注意字符集的设置要在获取输出流对象之前
         resp.setContentType("text/html;charset=UTF-8");
-        //1.获取字节输出流对象
-        ServletOutputStream sos = resp.getOutputStream();
-        //2.定义一个消息
-        String str = "字节流响应消息";
-        //3.通过字节流对象输出
-        //字符串数据通过UTF-8的字符集编码为字节数据
-        sos.write(str.getBytes("UTF-8"));
+        //获取字符输出流对象
+        PrintWriter pw = resp.getWriter();
+
+        pw.write(str);
     }
 
     @Override
