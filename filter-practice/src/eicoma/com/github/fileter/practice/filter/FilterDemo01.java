9cd34f0bb6a19a45dcd356a1513569d89f2c658e
Eicoma <328084834@qq.com>
2021-05-25 16:13:54 +0800

    FilterDemo01

--- filter-practice/src/eicoma/com/github/fileter/practice/filter/FilterDemo01.java
+++ filter-practice/src/eicoma/com/github/fileter/practice/filter/FilterDemo01.java
@@ -1,4 +1,29 @@
 package eicoma.com.github.fileter.practice.filter;
 
-public class FilterDemo01 {
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class FilterDemo01 implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("FilterDemo01执行了...");
+
+        //处理乱码
+        servletResponse.setContentType("text/html;charset=UTF-8");
+
+        //放行
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
